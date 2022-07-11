ecbab31ce508f8498c201bed8e982eb8adb839d9
Arthur Barbosa <arthurbruno03@gmail.com>
2022-07-11 13:37:43 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
         <h1>DSMeta</h1>
         <p>
           Desenvolvido por
-          <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+          <a href="https://github.com/arthurBarbosa"> Arthur Barbosa</a>
         </p>
       </div>
     </header>
