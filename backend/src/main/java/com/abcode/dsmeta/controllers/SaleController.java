bdbcecc0b3f9a7635ddf6598c6853179158d672c
Arthur Barbosa <arthurbruno03@gmail.com>
2022-07-13 10:26:53 -0300

    Date select

--- backend/src/main/java/com/abcode/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/abcode/dsmeta/controllers/SaleController.java
@@ -2,8 +2,11 @@ package com.abcode.dsmeta.controllers;
 
 import com.abcode.dsmeta.entities.Sale;
 import com.abcode.dsmeta.services.SaleService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,7 +22,8 @@ public class SaleController {
     }
 
     @GetMapping
-    public List<Sale> findSales(){
-        return saleService.findSales();
+    public Page<Sale> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                @RequestParam(value = "maxDate", defaultValue = "") String maxDate, Pageable pageable) {
+        return saleService.findSales(minDate, maxDate, pageable);
     }
 }
--- backend/src/main/java/com/abcode/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/abcode/dsmeta/services/SaleService.java
@@ -2,9 +2,13 @@ package com.abcode.dsmeta.services;
 
 import com.abcode.dsmeta.entities.Sale;
 import com.abcode.dsmeta.repositories.SaleRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
@@ -14,7 +18,11 @@ public class SaleService {
         this.saleRepository = saleRepository;
     }
 
-    public List<Sale> findSales() {
-        return saleRepository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        var min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        var max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+        return saleRepository.findSales(min, max, pageable);
     }
 }
